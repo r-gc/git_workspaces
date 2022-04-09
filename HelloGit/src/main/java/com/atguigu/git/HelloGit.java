5a78592461e61ac86f7d5fb6add500574b85d2e6
r-gc <your_email@youremail.com>
2022-04-09 13:08:24 +0800

    Initial commit

---
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+import java.sql.SQLOutput;
+
+/**
+ * @author 15116
+ * @date 2022/4/9 - 12:57
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("HelloGit");
+    }
+}
